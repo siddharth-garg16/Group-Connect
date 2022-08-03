59aa563d8c812a7102b4e260d4af991a09122661
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-08-03 21:40:07 +0530

    final commit

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from base import views
+from . import views
 
 urlpatterns = [
     path('', views.lobby),
     path('room/', views.room),
+    path('get_token/', views.getToken),
+
+    path('create_member/', views.createMember),
+    path('get_member/', views.getMember),
+    path('delete_member/', views.deleteMember),
 ]
