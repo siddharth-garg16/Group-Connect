1cdd3ed4a1e5596488ad967d2f802c2cbf936660
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-08-03 15:49:22 +0530

    control icons added

--- static/js/streams.js
+++ static/js/streams.js
@@ -9,6 +9,9 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
@@ -23,6 +26,33 @@ let joinAndDisplayLocalStream = async () => {
     localTracks[1].play(`user-${UID}`)
 
     await client.publish([localTracks[0], localTracks[1]])
+}
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+    if (mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player!=null){
+            player.remove()
+        }
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                  <div class="username-wrapper"><span class="user-name">MyName</span></div>
+                  <div class="video-player" id="user-${user.uid}"></div>
+                  </div>`
+
+        document.getElementById("video-streams").insertAdjacentHTML('beforeend', player)
+
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+}
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
 }
 
 joinAndDisplayLocalStream()
